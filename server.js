dd217e6b82acefd1377ec8a27d1fcab9dc41c28f
Kackary <zackattackers@gmail.com>
2025-11-13 13:52:14 -0500

    develop CRUD routes

--- server.js
+++ server.js
@@ -18,4 +18,33 @@ db.once("open", () => console.log("✅ Connected to MongoDB"));
 
 const Task = require("./models/Task");
 
+// create
+app.post("/add", async (req, res) => {
+  const task = new Task({
+    title: req.body.title,
+    completed: false,
+    dueDate: req.body.dueDate,
+  });
+  await task.save();
+  res.send("Task added!");
+});
+
+// read
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+// update
+app.post("/update/:id", async (req, res) => {
+  await Task.findByIdAndUpdate(req.params.id, req.body);
+  res.send("Task updated!");
+});
+
+// delete
+app.post("/delete/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.send("Task deleted!");
+});
+
 app.listen(3000, () => console.log("Server running at http://localhost:3000"));
